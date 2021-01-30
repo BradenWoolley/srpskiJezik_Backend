7d1bc8dbaa5e29fbc44f8777af2f3051e3ae6908
BW3538 <braden.mcw@gmail.com>
2021-01-31 00:43:53 +0200

    Added body-parser and response error message.

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require ('body-parser')
 const app = express()
 
+app.use(bodyParser.json());
 app.use(cors());
 const port = 8080;
 
-
 app.get('/', (req, res) => {
-    res.send(JSON.stringify('Test'));
+    res.send(JSON.stringify('Server is connected'));
 })
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    if (!req.body) {
+        return res.status(400).send({
+            message: 'Content can not be empty!'
+        });
+    }
+});
 
 app.listen(process.env.PORT || port, () => {
     console.log(`listening on ${port}`);
